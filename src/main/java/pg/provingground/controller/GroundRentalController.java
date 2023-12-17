66b791b290e18788dfbe1a9efc0603b54210824f
Hegale <juyeon@gimjuyeon-ui-MacBookAir.local>
2023-12-17 23:52:15 +0900

    feat(ground-rental): 실시간 대여현황 적용 및 해당 날짜의 날씨 확인 기능 추가

--- src/main/java/pg/provingground/controller/GroundRentalController.java
+++ src/main/java/pg/provingground/controller/GroundRentalController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pg.provingground.domain.Ground;
 import pg.provingground.domain.User;
 import pg.provingground.dto.history.GroundRentalHistory;
 import pg.provingground.repository.UserRepository;
