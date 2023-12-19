88416fb7500e8cca257c431b5d7d89b7b3095a7c
Hegale <juyeon@gimjuyeon-ui-MacBookAir.local>
2023-12-20 06:39:12 +0900

    test(car): car repository 테스트 추가

--- src/test/java/pg/provingground/repository/CarRepositoryTest.java
+++ src/test/java/pg/provingground/repository/CarRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
 import pg.provingground.domain.Car;
 import pg.provingground.domain.CarType;
 
@@ -20,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarRepositoryTest {
 
     @Autowired private CarRepository carRepository;
+    @Autowired private CarTypeRepository carTypeRepository;
     private CarType carType;
     private Car car;
 
@@ -33,6 +35,7 @@ class CarRepositoryTest {
     public void 차량번호로_검색_성공() throws Exception {
         //given
         String carNumber = "92고 3761";
+        carTypeRepository.save(carType);
         carRepository.save(car);
 
         //when
@@ -46,6 +49,7 @@ class CarRepositoryTest {
     public void 차량번호로_검색_실패() throws Exception {
         //given
         String carNumber = "92고 3762";
+        carTypeRepository.save(carType);
         carRepository.save(car);
 
         //when
@@ -58,6 +62,7 @@ class CarRepositoryTest {
     @Test
     public void 차종으로_검색() throws Exception {
         //given
+        carTypeRepository.save(carType);
         carRepository.save(car);
         List<CarType> carTypes = List.of(carType);
 
@@ -70,17 +75,30 @@ class CarRepositoryTest {
     }
     
     @Test
-    public void () throws Exception {
+    public void 차량번호_중복_확인() throws Exception {
         //given
-        
+        carTypeRepository.save(carType);
+        carRepository.save(car);
+        String carNumber = "92고 3762";
+
         //when
-        
         //then
-        
+        assertTrue(carRepository.isDuplicateCarNumber(carNumber));
     }
 
+    @Test
+    public void 차종별_차량대수() throws Exception {
+        //given
+        // ApplicationContext를 통해 CarTypeRepository 빈을 가져옴
+        carTypeRepository.save(carType);
+        Car car2 = Car.createCar(carType, "31바 8241");
+        carRepository.save(car);
+        carRepository.save(car2);
 
+        //when
+        //then
+        assertEquals(carRepository.countCarsPerCarType(1L), 2);
 
-
+    }
 
 }
